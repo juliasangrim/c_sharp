0678a799beb6b13abc791b0bdeeeef31370c2a85
julia <y.trubitsyna@g.nsu.ru>
2022-09-23 13:34:37 +0700

    add summary to method 'GenerateNames'

--- princess_choice/princess_choice/generator/ContenderNameGenerator.cs
+++ princess_choice/princess_choice/generator/ContenderNameGenerator.cs
@@ -2,17 +2,22 @@
 
 public class ContenderNameGenerator
 {
-    private static string[] _firstName = {"Ben", "Bart", "Carter", "Rufus", "Dan", "Chuck", "Nate", "Eric", "Jack", "Cyrus"};
-    private static string[] _lastName = {"Donovan", "Bass", "Humphrey", " Baizen", "Rose", "Archibald", "van der Woodsen", "Waldorf", "Sparks", "Dickens"};
+    private static string[] _firstNames = {"Ben", "Bart", "Carter", "Rufus", "Dan", "Chuck", "Nate", "Eric", "Jack", "Cyrus"};
+    private static string[] _lastNames = {"Donovan", "Bass", "Humphrey", " Baizen", "Rose", "Archibald", "van der Woodsen", "Waldorf", "Sparks", "Dickens"};
     
-    
+    /// <summary>
+    /// This method generate name for contenders.
+    /// </summary>
+    /// <returns>
+    /// Returns 100 generated contender names 
+    /// </returns>
     public static List<string> GenerateNames()
     {
         var contenderNames = (
-            from fName in _firstName 
-            from sName in _lastName
+            from fName in _firstNames 
+            from sName in _lastNames
             select $"{fName} {sName}").ToList();
         return contenderNames;
-    }
+    }   
     
 }
