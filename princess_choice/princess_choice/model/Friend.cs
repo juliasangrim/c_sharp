a60f3aaf3bd09b0d92713efc25a6fa3104b38f80
julia <y.trubitsyna@g.nsu.ru>
2022-09-25 19:20:20 +0700

    class contender change to interface in methods signature

--- princess_choice/princess_choice/model/Friend.cs
+++ princess_choice/princess_choice/model/Friend.cs
@@ -1,45 +1,56 @@
 ﻿namespace princess_choice;
 
-public class Friend
+public class Friend : IFriend
 {
+    /// <summary>
+    /// The best contender of all passed contenders.
+    /// </summary>
+    private IContender? _lastBestContender;
+
     /// <summary>
     /// The list of passed contenders.
     /// </summary>
-    public List<Contender> PassedContenders { get; }
+    public List<IContender> PassedContenders { get; }
 
     public Friend()
     {
-        PassedContenders = new List<Contender>();
+        _lastBestContender = null;
+        PassedContenders = new List<IContender>();
     }
 
     /// <summary>
-    /// Add contender in list of passed contenders.
+    /// Add contender in list of passed contenders and choose the best of them.
     /// </summary>
     /// <param name="passedContender"> Contender rejected by the princess.</param>
-    public void AddPassedContender(Contender passedContender)
+    public void AddPassedContender(IContender passedContender)
     {
+        if (_lastBestContender == null ||
+            _lastBestContender.Value() < passedContender.Value())
+        {
+            _lastBestContender = passedContender;
+        }
+
         PassedContenders.Add(passedContender);
     }
-    
+
     /// <summary>
     /// Detect the best contender.
     /// </summary>
     /// <param name="currContender">The contender who is with the princess now.</param>
-    /// <param name="lastBestContender">The best contender of all the has passed</param>
     /// <returns>Returns true - if contender with the princess is better than last best contender,
     /// otherwise - return false</returns>
-    public bool IsCurrContenderBest(Contender currContender, Contender? lastBestContender)
+    public bool IsCurrContenderBest(IContender currContender)
     {
-        if (lastBestContender == null)
+        if (_lastBestContender == null)
         {
             return true;
         }
-        
-        if (!PassedContenders.Contains(lastBestContender))
+
+        if (!PassedContenders.Contains(_lastBestContender))
         {
             throw new ArgumentException("Contender not in list of passed contenders!");
         }
-        
-        return lastBestContender.Value < currContender.Value;
+
+        return _lastBestContender.Value() < currContender.Value();
     }
 }
