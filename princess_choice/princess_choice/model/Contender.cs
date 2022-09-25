37a02371b4297581e7a8d9b0b8a5fabc41d440fc
julia <y.trubitsyna@g.nsu.ru>
2022-09-25 15:25:34 +0700

    add summary, change field name

--- princess_choice/princess_choice/model/Contender.cs
+++ princess_choice/princess_choice/model/Contender.cs
@@ -2,22 +2,30 @@
 
 public class Contender
 {
+    /// <summary>
+    /// Contender name, consist of first name and second name
+    /// Name is unique.
+    /// </summary>
     private string _name;
-    private int _matchScore;
+    
+    /// <summary>
+    /// Contender value, show how cool he is.
+    /// </summary>
+    private int _value;
 
-    public Contender(string name, int matchScore)
+    public Contender(string name, int value)
     {
-        _matchScore = matchScore;
+        _value = value;
         _name = name;
+    }
+
+    public int Value
+    {
+        get => _value;
     }
 
     public override string ToString()
     {
         return _name;
-    }  
-    
-    public int MatchScore
-    {
-        get => _matchScore;
     }
 }
