1a7ae3c3b0e296d895afbef3f2d074e438436fb9
julia <y.trubitsyna@g.nsu.ru>
2022-09-25 19:16:56 +0700

    Overriding interface method Value

--- princess_choice/princess_choice/model/Contender.cs
+++ princess_choice/princess_choice/model/Contender.cs
@@ -1,27 +1,31 @@
 ﻿namespace princess_choice;
 
-public class Contender
+public class Contender : IContender
 {
     /// <summary>
     /// Contender name, consist of first name and second name
     /// Name is unique.
     /// </summary>
     private readonly string _name;
-    
+
     /// <summary>
     /// Contender value, show how cool he is.
     /// </summary>
-    public int Value { get; }
+    public int _value;
 
     public Contender(string name, int value)
     {
         _name = name;
-        Value = value;
+        _value = value;
     }
-
-   
+    
     public override string ToString()
     {
         return _name;
+    }
+
+    public int Value()
+    {
+        return _value;
     }
 }
