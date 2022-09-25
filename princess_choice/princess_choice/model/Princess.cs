d0788852338a5c143b3d30204d92b9a8c149dd8e
julia <y.trubitsyna@g.nsu.ru>
2022-09-25 19:24:08 +0700

    Add summary, method CountHappy split into 2 methods, change all in princess to interface

--- princess_choice/princess_choice/model/Princess.cs
+++ princess_choice/princess_choice/model/Princess.cs
@@ -2,30 +2,42 @@
 
 public class Princess
 {
-    private IStrategy _strategy;
-    private Contender? _prince;
-    public Princess(Friend friend, Hall hall)
+    /// <summary>
+    /// Princess choose strategy  
+    /// </summary>
+    private readonly IStrategy _strategy;
+
+    /// <summary>
+    /// The prince chosen by the princess
+    /// </summary>
+    private IContender? _prince;
+
+    public Princess(IFriend friend, IHall hall)
     {
-       
         _strategy = new Strategy(friend, hall);
     }
-    
+
+    /// <summary>
+    /// Choose the prince by princess.
+    /// </summary>
+    public void ChoosePrince()
+    {
+        _prince = _strategy.BestContender();
+    }
+
+    /// <summary>
+    /// Count the happiness of a princess after choosing a prince.
+    /// </summary>
+    /// <returns>Returns 0 - if the princess choose the bad prince,
+    /// returns 10 - if the princess did not choose the prince,
+    /// returns from 100, 99, ... 51 - if princess chose the 1st good prince,
+    /// 2nd good prince ... 50th best prince. </returns>
     public int CountHappy()
     {
-        var happiness = 0;
-        _prince = _strategy.BestContender();
-        if (_prince != null)
-        {
-            if (_prince.MatchScore > IStrategy.MaxMatchScore / 2 )
-            {
-                happiness = _prince.MatchScore;
-            }
-        }
-        else
-        {
-            happiness = 10;
-        }
+        var happiness = 10;
+        if (_prince == null) return happiness;
 
+        happiness = _prince.Value() > IStrategy.MaxMatchScore / 2 ? _prince.Value() : 0;
         return happiness;
     }
 }
