d91c2e6d1399509c4b3ad84a3ecd721a049d9184
julia <y.trubitsyna@g.nsu.ru>
2022-09-25 15:59:58 +0700

    Add summary, change method isHeBest, add check

--- princess_choice/princess_choice/model/Friend.cs
+++ princess_choice/princess_choice/model/Friend.cs
@@ -2,26 +2,44 @@
 
 public class Friend
 {
+    /// <summary>
+    /// The list of passed contenders.
+    /// </summary>
+    public List<Contender> PassedContenders { get; }
+
     public Friend()
     {
         PassedContenders = new List<Contender>();
     }
 
-    public List<Contender> PassedContenders { get; }
-
-    public void AddPassedContender(Contender rejectedContender)
+    /// <summary>
+    /// Add contender in list of passed contenders.
+    /// </summary>
+    /// <param name="passedContender"> Contender rejected by the princess.</param>
+    public void AddPassedContender(Contender passedContender)
     {
-        PassedContenders.Add(rejectedContender);
+        PassedContenders.Add(passedContender);
     }
-
-    public bool IsHeBest(Contender contender, Contender? bestContender)
+    
+    /// <summary>
+    /// Detect the best contender.
+    /// </summary>
+    /// <param name="currContender">The contender who is with the princess now.</param>
+    /// <param name="lastBestContender">The best contender of all the has passed</param>
+    /// <returns>Returns true - if contender with the princess is better than last best contender,
+    /// otherwise - return false</returns>
+    public bool IsCurrContenderBest(Contender currContender, Contender? lastBestContender)
     {
-        if (bestContender == null)
+        if (lastBestContender == null)
         {
             return true;
         }
-
-        return bestContender.MatchScore < contender.MatchScore;
+        
+        if (!PassedContenders.Contains(lastBestContender))
+        {
+            throw new ArgumentException("Contender not in list of passed contenders!");
+        }
+        
+        return lastBestContender.Value < currContender.Value;
     }
-
 }
