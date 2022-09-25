1d265225564659d767c1b4f69ca527b29c683e58
julia <y.trubitsyna@g.nsu.ru>
2022-09-25 19:22:04 +0700

    class contender change to interface in methods signature

--- princess_choice/princess_choice/model/Hall.cs
+++ princess_choice/princess_choice/model/Hall.cs
@@ -1,46 +1,48 @@
 ﻿namespace princess_choice;
 
-public class Hall 
+public class Hall : IHall
 {
     /// <summary>
     /// List of all waiting contenders.
     /// </summary>
-    private readonly List<Contender> _allContenders;
+    private readonly List<IContender> _allContenders;
+
     /// <summary>
     /// Enumerator for list of waiting contenders.
     /// </summary>
-    private List<Contender>.Enumerator _enumerator;
+    private List<IContender>.Enumerator _enumerator;
 
     public Hall()
     {
-        _allContenders = new List<Contender>();
+        _allContenders = new List<IContender>();
         var contenderNames = ContenderNameGenerator.GenerateNames();
         for (var i = 1; i <= contenderNames.Count; ++i)
         {
             _allContenders.Add(new Contender(contenderNames[i - 1], i));
         }
+
         MixContenders();
         _enumerator = _allContenders.GetEnumerator();
     }
-    
+
     /// <summary>
     /// Shuffle list of waiting contenders.
     /// </summary>
     private void MixContenders()
     {
-        var random = new Random(DateTime.Now.Millisecond);  
+        var random = new Random(DateTime.Now.Millisecond);
         for (int i = _allContenders.Count - 1; i >= 0; --i)
         {
             var randomIndex = random.Next(_allContenders.Count);
             (_allContenders[i], _allContenders[randomIndex]) = (_allContenders[randomIndex], _allContenders[i]);
         }
     }
-  
+
     /// <summary>
     /// Get next waiting contenders.
     /// </summary>
     /// <returns>Returns next waiting contender if exist, otherwise return null.</returns>
-    public Contender? NextContender()
+    public IContender? NextContender()
     {
         if (_enumerator.MoveNext())
         {
